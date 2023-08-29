5823efece9312f2baf51d93e8818b24e14694ec4
Tzuzu <natoiodice@gmail.com>
2023-08-29 03:52:47 -0700

    Added more changes

--- fortunato-portfolio-v1.0/src/Page/Resume.js
+++ fortunato-portfolio-v1.0/src/Page/Resume.js
@@ -1,7 +1,33 @@
 import React from 'react'
 
 const Resume = () => {
-  return <div>Resume</div>
+  return <div className="resume">
+    <p>
+      Download my <a href="https://www.linkedin.com">resume</a>
+    </p>
+    <div className="row mt-5">
+      <div className="col-10 col-md-5 d-flex flex-column align-items-center mb-5">
+        <h3>Front-end Knowledge</h3>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Bootstrap</li>
+          <li class="list-group-item">CSS</li>
+          <li class="list-group-item">HTML</li>
+          <li class="list-group-item">Javascript</li>
+          <li class="list-group-item">React</li>
+        </ul>
+      </div>
+      <div className="col-10 col-md-5 d-flex flex-column align-items-center">
+        <h3>Back-end Knowledge</h3>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">API</li>
+          <li class="list-group-item">Node</li>
+          <li class="list-group-item">Express</li>
+          <li class="list-group-item">MySQL</li>
+          <li class="list-group-item">Mongoose, MongoDB</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 }
 
 export default Resume
