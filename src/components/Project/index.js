ee3218555bb1004f738c9b0734d19024835baefb
Tzuzu <natoiodice@gmail.com>
2023-08-29 17:41:34 -0700

    Fixed CSS

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,26 +3,19 @@ import React from 'react'
 const Project = ({ project }) => {
   return (
     <div className='col'>
+      <h5>{project.name}</h5>
       <a href={project.link}>
         <div class="card">
           <img src={require(`../../assets/projects/${project.name}.jpg`)} 
           class="card-img" alt={project.name}/>
-          <div class="card-img-overlay image">
-            <h5 class="card-title">
-                <a href={project.link} className='card-text'>
-                    {project.name}
-                </a>
-                <a href={project.repo} className='card-text'>
-                </a>
-            </h5>
           </div>
+        </a>
+          <br></br>
           <div>
             <p class="card-text description">
                   {project.description}
             </p>
           </div>
-        </div>
-        </a>
     </div>
   )
 }
