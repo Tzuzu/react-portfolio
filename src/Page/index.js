c79bad9744be6afeae702e2d398e338f979983a8
Tzuzu <natoiodice@gmail.com>
2023-08-31 05:13:35 -0700

    Final Commit

--- src/Page/index.js
+++ src/Page/index.js
@@ -3,7 +3,6 @@ import About from './About'
 import Portfolio from './Portfolio'
 import Contact from './Contact'
 import Resume from './Resume'
-import {capitalize} from '../utils'
 import './style.css'
 
 const Page = ({currentPage}) => {
