3402cd90d164a10c77deb2c1f5cf7aca4b9e581c
Tzuzu <natoiodice@gmail.com>
2023-08-31 16:32:26 -0700

    Fixed Nav.js

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,10 +5,10 @@ import './style.css';
 const Nav = ({pages, currentPage, setCurrentPage}) => {
   return <nav className="nav d-flex justify-content-between align-items-center">
     {pages.map((page) => (
-        <a href="/#" key={page} className={`nav-link ${page === currentPage && 'active'}`}
+        <li key={page} className={`nav-link ${page === currentPage && 'active'}`}
         onClick={() => setCurrentPage(page)}>
             {capitalize(page)}
-        </a>
+        </li>
     ))}
   </nav>
 }
